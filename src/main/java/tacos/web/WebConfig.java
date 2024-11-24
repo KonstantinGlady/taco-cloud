b706dd7c7dfe7dc1919693894b18a77e2137f534
Glady <gladyi@hotmail.com>
2024-11-24 19:40:42 +0300

    ch 2 Validation, WebMvcConfigurator

---
+++ src/main/java/tacos/web/WebConfig.java
@@ -0,0 +1,28 @@
+package tacos.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+}
+
+//for example it's possible to  implement in application class
+/*@SpringBootApplication
+public class TacoCloudApplication implements WebMvcConfigurer {
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+}*/
+
